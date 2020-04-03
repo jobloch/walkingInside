7387b67c8b3270eb86256b7a1363592a066710ee
jobloch <63001298+jobloch@users.noreply.github.com>
2020-04-03 12:29:20 -0400

    index

---
+++ script.js
@@ -0,0 +1,12 @@
+var x = 0;
+var lapLength=55;
+document.onkeypress=function(event){
+	if(event.keyCode==32 || event.keyCode == 61){
+		x++;
+	}
+	if(event.keyCode==45){
+		x--;
+	}
+	document.getElementById('laps').innerHTML=x;
+	document.getElementById('mileage').innerHTML=Math.round(1000*(x*lapLength)/5280)/1000
+}
